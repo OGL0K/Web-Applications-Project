47580f539300fa8281df21cf3c4918066a5b66ac
OGL0K <oguzgokyuzu01@gmail.com>
2021-11-14 15:20:26 +0000

    Added home page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,14 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+
+    assert_select 'title', 'Spotifay'
+    assert_select 'h1', 'Spotifay'
+    assert_select 'p', 'Welcome to Spotifay!'
+    assert_select 'p', 'The easiest way of adding your songs and albums.'
+
   end
 
 end
