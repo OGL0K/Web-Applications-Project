afda94e2291011b0a55cdb00cee160377704cf12
OGL0K <oguzgokyuzu01@gmail.com>
2021-11-25 11:07:11 +0000

    Added songs title index

--- db/migrate/20211125105225_create_songs.rb
+++ db/migrate/20211125105225_create_songs.rb
@@ -1,10 +1,10 @@
 class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
-      t.string :title
-      t.string :artist
-      t.string :album
-      t.string :duration
+      t.string :title, null:false
+      t.string :artist, null:false
+      t.string :album, null: false
+      t.string :duration, null: false
 
       t.timestamps
     end
