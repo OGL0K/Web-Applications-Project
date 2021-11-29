19c40b0c75e87e2112038a20495ea2efaa4751d9
OGL0K <oguzgokyuzu01@gmail.com>
2021-11-29 22:31:54 +0000

    Added 2 more mailers and tests which all pass

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -4,9 +4,23 @@ class ContactMailerTest < ActionMailer::TestCase
 
   test "should return contact email" do
     mail = ContactMailer.contact_email("oguzgokyuzu@me.com",
-      "Oguz Gokyuzu", "345631230", @message = "Hello")
+      "Oguz Gokyuzu", "345631230", @message = "Will you add different themes for this application?")
       assert_equal ['info@mynotes.com'], mail.to
       assert_equal ['info@mynotes.com'], mail.from
     end
 
+    test "should return contact email2" do
+      mail = ContactMailer.contact_email("burcupe@gmail.com",
+        "Burcu Parlaksu", "1234567890", @message = "Hello I am having problems when I try to add a song please help me.")
+
+      assert_equal ['info@mynotes.com'], mail.to
+      assert_equal ['info@mynotes.com'], mail.from
+  end
+  test "should return contact email3" do
+    mail = ContactMailer.contact_email("mertcoban@gmail.com",
+      "Mert Coban", "0978456321", @message = "I am having issues when I try to add a note to a song I need help. Thank you.")
+
+    assert_equal ['info@mynotes.com'], mail.to
+    assert_equal ['info@mynotes.com'], mail.from
+  end
 end
--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -3,7 +3,17 @@ class ContactMailerPreview < ActionMailer::Preview
 
   def contact_email
     ContactMailer.contact_email("oguzgokyuzu@me.com",
-       "Oguz Gokyuzu", "345631230", @message = "Hello")
+       "Oguz Gokyuzu", "345631230", @message = "Will you add different themes for this application?")
   end
+
+  def contact_email2
+    ContactMailer.contact_email("burcupe@gmail.com",
+      "Burcu Parlaksu", "1234567890", @message = "Hello I am having problems when I try to add a song please help me.")
+    end
+
+    def contact_email3
+      ContactMailer.contact_email("mertcoban@outlook.com",
+        "Mert Coban", "0978456321", @message = "I am having issues when I try to add a note to a song I need help. Thank you.")
+      end
 
 end
